b9ac063207f5f7c8e3e1d77597764948064a5b46
NewGen2022 <amirgimari@gmail.com>
2024-07-05 12:39:00 +0300

    init commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# mock-shopping-cart
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+    return (
+        <>
+            <h1>Mock Shopping Cart</h1>
+        </>
+    );
+}
+
+export default App;
