bac1290ba8a72ed2a37df10ddd9e1f953c43e439
NewGen2022 <amirgimari@gmail.com>
2024-07-05 12:43:49 +0300

    added base to vite.config and changed site title

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/',
+    base: '/mock-shopping-cart/',
     plugins: [react()],
     server: {
         port: 3000,
