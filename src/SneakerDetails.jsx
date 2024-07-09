142f04f3225e49834e3a23efb874c3d79f1790dc
NewGen2022 <amirgimari@gmail.com>
2024-07-09 21:12:12 +0300

    added displaying of specific sneaker according of its id

--- src/SneakerDetails.jsx
+++ src/SneakerDetails.jsx
@@ -1,7 +1,41 @@
+import { useLocation } from 'react-router-dom';
+import { useState } from 'react';
+import './styles/SneakerDetails.css';
+
 const SneakerDetails = () => {
+    const location = useLocation();
+    const shoe = location.state || false;
+
+    const [numberOfItem, setNumberOfItem] = useState(1);
+
+    const handlePlusItem = () => {
+        setNumberOfItem(prevState => prevState + 1);
+    };
+
+    const handleMinusItem = () => {
+        setNumberOfItem(prevState => (prevState > 1 ? prevState - 1 : 1));
+    };
+
     return (
-        <div>
-            SNEAKER DETAILS
+        <div className='shoe-details'>
+            {shoe ? (
+                <>
+                    <img src={shoe.imageUrl} />
+                    <div className='details'>
+                        <div className='sneaker-name'>{shoe.name}</div>
+                        <div className='sneaker-price'>{shoe.price}</div>
+                        <div className='sneaker-description'>{shoe.description}</div>
+                        <div className='number-of-item-container'>
+                            <button onClick={handleMinusItem}>-</button>
+                            <div className='number-of-item'>{numberOfItem}</div>
+                            <button onClick={handlePlusItem}>+</button>
+                        </div>
+                        <button className='add-to-cart'>+ Add to cart</button>
+                    </div>
+                </>
+            ) : (
+                <p>No sneaker data available.</p>
+            )}
         </div>
     );
 };
