bd287b5ab24a577734bde75d5328d87195ecc7fa
NewGen2022 <amirgimari@gmail.com>
2024-07-10 09:52:46 +0300

    added handler for adding item to cart

--- src/SneakerDetails.jsx
+++ src/SneakerDetails.jsx
@@ -1,12 +1,15 @@
 import { useLocation } from 'react-router-dom';
 import { useState } from 'react';
+import PropTypes from 'prop-types';
 import './styles/SneakerDetails.css';
+import PopOutWindow from './PopOutWindow';
 
-const SneakerDetails = () => {
+const SneakerDetails = ({ setCartItems }) => {
     const location = useLocation();
     const shoe = location.state || false;
 
     const [numberOfItem, setNumberOfItem] = useState(1);
+    const [addedToCart, setAddedToCart] = useState(false);
 
     const handlePlusItem = () => {
         setNumberOfItem(prevState => prevState + 1);
@@ -16,11 +19,26 @@ const SneakerDetails = () => {
         setNumberOfItem(prevState => (prevState > 1 ? prevState - 1 : 1));
     };
 
+    const addToCart = () => {
+        setCartItems(prevItems => {
+            const itemIndex = prevItems.findIndex(item => item.id === shoe.id);
+            setAddedToCart(true);
+            if (itemIndex > -1) {
+                const newItems = [...prevItems];
+                newItems[itemIndex].numberOfItem += numberOfItem;
+
+                return newItems;
+            } else {
+                return [...prevItems, { ...shoe, numberOfItem }];
+            }
+        });
+    };
+
     return (
         <div className='shoe-details'>
             {shoe ? (
                 <>
-                    <img src={shoe.imageUrl} />
+                    <img src={shoe.imageUrl} alt={shoe.name} />
                     <div className='details'>
                         <div className='sneaker-name'>{shoe.name}</div>
                         <div className='sneaker-price'>{shoe.price}</div>
@@ -30,7 +48,8 @@ const SneakerDetails = () => {
                             <div className='number-of-item'>{numberOfItem}</div>
                             <button onClick={handlePlusItem}>+</button>
                         </div>
-                        <button className='add-to-cart'>+ Add to cart</button>
+                        <button className='add-to-cart' onClick={addToCart}>+ Add to cart</button>
+                        {addedToCart && <PopOutWindow />}
                     </div>
                 </>
             ) : (
@@ -38,6 +57,10 @@ const SneakerDetails = () => {
             )}
         </div>
     );
+};
+
+SneakerDetails.propTypes = {
+    setCartItems: PropTypes.func.isRequired,
 };
 
 export default SneakerDetails;
