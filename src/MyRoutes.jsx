cd652324d64c01b52188863a19c4058e2e921d17
NewGen2022 <amirgimari@gmail.com>
2024-07-10 09:53:24 +0300

    added props for handling cart items adding and displaying

--- src/MyRoutes.jsx
+++ src/MyRoutes.jsx
@@ -1,12 +1,13 @@
 import { useEffect } from 'react';
 import { Route, Routes, useLocation } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import './styles/Home.css';
 import Home from './Home';
 import Sneakers from './Sneakers';
 import SneakerDetails from './SneakerDetails';
 import Cart from './Cart';
 
-const MyRoutes = () => {
+const MyRoutes = ({ cartItems, setCartItems }) => {
     const location = useLocation();
 
     useEffect(() => {
@@ -21,10 +22,15 @@ const MyRoutes = () => {
         <Routes>
             <Route path='/mock-shopping-cart/' element={ <Home /> } />
             <Route path='/mock-shopping-cart/sneakers/' element={ <Sneakers /> } />
-            <Route path='/mock-shopping-cart/sneakers/:id' element={ <SneakerDetails /> } />
-            <Route path='/mock-shopping-cart/cart/' element={ <Cart /> } />
+            <Route path='/mock-shopping-cart/sneakers/:id' element={ <SneakerDetails setCartItems={setCartItems} /> } />
+            <Route path='/mock-shopping-cart/cart/' element={ <Cart items={cartItems} /> } />
         </Routes>
     );
+};
+
+MyRoutes.propTypes = {
+    cartItems: PropTypes.array.isRequired,
+    setCartItems: PropTypes.func.isRequired,
 };
 
 export default MyRoutes;
